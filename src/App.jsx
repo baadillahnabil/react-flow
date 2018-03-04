26b2763208841a9272f6921bc6539a45c9fe7e0c
Nabil Baadillah <baadillahnabil@gmail.com>
2018-03-04 18:33:58 +0700

    learn to use state

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,37 @@ import Person from './Components/Person/Person'
 
 /* $FlowIgnore: suppressing this error */
 export default class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Nabil',
+        age: 22
+      },
+      {
+        name: 'Budo',
+        age: 15
+      },
+      {
+        name: 'Brian Neil',
+        age: 57
+      }
+    ]
+  }
+
   render() {
+    const { persons } = this.state
+
     return (
       <Fragment>
         <div className="App">
           <p>Welcome To React</p>
-          <Person name="Nabil" age="22">
+          <Person name={persons[0].name} age={persons[0].age}>
             "You know who i am"
           </Person>
-          <Person name="Budo" age="12">
+          <Person name={persons[1].name} age={persons[1].age}>
             "You know who i am"
           </Person>
-          <Person name="Brian O'Neil" age="43" />
+          <Person name={persons[2].name} age={persons[2].age} />
         </div>
       </Fragment>
     )
