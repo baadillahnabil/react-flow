9fc752a882dc40c1ae99c96b8f4a5b0864a5cab0
Nabil Baadillah <baadillahnabil@gmail.com>
2018-03-07 17:19:20 +0700

    learn emit event and inline styling

--- src/Components/Person/Person.jsx
+++ src/Components/Person/Person.jsx
@@ -1,16 +1,43 @@
 /* @flow */
 
 import React, { Component, Fragment } from 'react'
+import './Person.css'
 
 // $FlowIgnore: suppressing this error
 export default class Person extends Component {
   render() {
+    // Inline Styling
+    const style: Object = {
+      removeBtn: {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid red',
+        padding: '5px',
+        cursor: 'pointer'
+      },
+      inputName: {
+        font: 'inherit',
+        padding: '5px'
+      }
+    }
+
     return (
       <Fragment>
-        <p>
-          My Name is {this.props.name} and my age is {this.props.age}
-        </p>
-        {this.props.children}
+        <div className="Person">
+          <button style={style.removeBtn} onClick={this.props.onRemoveClicked}>
+            Remove This Person
+          </button>
+          <p>
+            My Name is {this.props.name} and my age is {this.props.age}
+          </p>
+          <input
+            style={style.inputName}
+            type="text"
+            onChange={this.props.onInputChanged}
+            value={this.props.name}
+          />
+          {this.props.children}
+        </div>
       </Fragment>
     )
   }
